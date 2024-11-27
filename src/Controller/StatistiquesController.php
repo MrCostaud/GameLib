89cf22bb03818dffddd066bb89db7ec62e14fc6c
MrCostaud <simarikalban@gmail.com>
2024-11-27 10:47:08 +0100

    push

---
+++ src/Controller/StatistiquesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class StatistiquesController extends AbstractController
+{
+    #[Route('/statistiques', name: 'statistiques')]
+    public function index(): Response
+    {
+        return $this->render('statistiques/index.html.twig', [
+            'controller_name' => 'StatistiquesController',
+        ]);
+    }
+}
