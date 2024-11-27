4f8f0446d3b2f61c6d8e4342cf641fe32449b36b
LangBapt <baptistelanglois0@gmail.com>
2024-11-27 12:18:01 +0100

    <feat><jeu d'essai bdd>
    <ajout d'un jeu d'essai pour la base de données>

--- gamelib.sql
+++ gamelib.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : mer. 27 nov. 2024 à 09:43
+-- Généré le : mer. 27 nov. 2024 à 11:13
 -- Version du serveur : 8.3.0
 -- Version de PHP : 8.2.18
 
@@ -30,13 +30,22 @@ SET time_zone = "+00:00";
 DROP TABLE IF EXISTS `amiibo`;
 CREATE TABLE IF NOT EXISTS `amiibo` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `post_id` int DEFAULT NULL,
+  `etat_amiibo_id` int DEFAULT NULL,
   `id_amiibo` int NOT NULL,
   `nom_amiibo` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `img_amiibo` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `IDX_9826F36A4B89032C` (`post_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `IDX_9826F36A3E0FFA31` (`etat_amiibo_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `amiibo`
+--
+
+INSERT INTO `amiibo` (`id`, `etat_amiibo_id`, `id_amiibo`, `nom_amiibo`, `img_amiibo`) VALUES
+(1, 1, 301, 'Link - Archer', 'link_archer.png'),
+(2, 2, 302, 'Mario - Classic', 'mario_classic.png'),
+(3, 3, 303, 'Samus - Varia Suit', 'samus_varia.png');
 
 -- --------------------------------------------------------
 
@@ -47,34 +56,22 @@ CREATE TABLE IF NOT EXISTS `amiibo` (
 DROP TABLE IF EXISTS `console`;
 CREATE TABLE IF NOT EXISTS `console` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `post_id` int DEFAULT NULL,
+  `etat_console_id` int DEFAULT NULL,
   `id_console` int NOT NULL,
   `nom_console` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `img_console` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `IDX_3603CFB64B89032C` (`post_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
+  KEY `IDX_3603CFB6DBB5E18A` (`etat_console_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Structure de la table `doctrine_migration_versions`
+-- Déchargement des données de la table `console`
 --
 
-DROP TABLE IF EXISTS `doctrine_migration_versions`;
-CREATE TABLE IF NOT EXISTS `doctrine_migration_versions` (
-  `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `executed_at` datetime DEFAULT NULL,
-  `execution_time` int DEFAULT NULL,
-  PRIMARY KEY (`version`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
-
---
--- Déchargement des données de la table `doctrine_migration_versions`
---
-
-INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
-('DoctrineMigrations\\Version20241127093632', '2024-11-27 09:37:00', 1037);
+INSERT INTO `console` (`id`, `etat_console_id`, `id_console`, `nom_console`, `img_console`) VALUES
+(1, 1, 101, 'Nintendo Switch', 'nintendo_switch.png'),
+(2, 2, 102, 'PlayStation 5', 'playstation_5.png'),
+(3, 3, 103, 'Xbox Series X', 'xbox_series_x.png');
 
 -- --------------------------------------------------------
 
@@ -87,7 +84,16 @@ CREATE TABLE IF NOT EXISTS `etat` (
   `id` int NOT NULL AUTO_INCREMENT,
   `etat` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `etat`
+--
+
+INSERT INTO `etat` (`id`, `etat`) VALUES
+(1, 'complet'),
+(2, 'incomplet'),
+(3, 'dégradé');
 
 -- --------------------------------------------------------
 
@@ -98,15 +104,24 @@ CREATE TABLE IF NOT EXISTS `etat` (
 DROP TABLE IF EXISTS `jeu`;
 CREATE TABLE IF NOT EXISTS `jeu` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `post_id` int DEFAULT NULL,
-  `postjeu_id` int NOT NULL,
+  `console_jeu_id` int DEFAULT NULL,
+  `etat_jeu_id` int NOT NULL,
   `id_jeu` int NOT NULL,
   `nom_jeu` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `img_jeu` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `IDX_82E48DB54B89032C` (`post_id`),
-  KEY `IDX_82E48DB591BD595D` (`postjeu_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  KEY `IDX_82E48DB54456D5FB` (`console_jeu_id`),
+  KEY `IDX_82E48DB53E65A52A` (`etat_jeu_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Déchargement des données de la table `jeu`
+--
+
+INSERT INTO `jeu` (`id`, `console_jeu_id`, `etat_jeu_id`, `id_jeu`, `nom_jeu`, `img_jeu`) VALUES
+(1, 1, 1, 201, 'The Legend of Zelda: Breath of the Wild', 'zelda_botw.png'),
+(2, 2, 2, 202, 'Spider-Man: Miles Morales', 'spiderman_mm.png'),
+(3, 3, 3, 203, 'Halo Infinite', 'halo_infinite.png');
 
 -- --------------------------------------------------------
 
@@ -130,20 +145,20 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Contraintes pour la table `amiibo`
 --
 ALTER TABLE `amiibo`
-  ADD CONSTRAINT `FK_9826F36A4B89032C` FOREIGN KEY (`post_id`) REFERENCES `etat` (`id`);
+  ADD CONSTRAINT `FK_9826F36A3E0FFA31` FOREIGN KEY (`etat_amiibo_id`) REFERENCES `etat` (`id`);
 
 --
 -- Contraintes pour la table `console`
 --
 ALTER TABLE `console`
-  ADD CONSTRAINT `FK_3603CFB64B89032C` FOREIGN KEY (`post_id`) REFERENCES `etat` (`id`);
+  ADD CONSTRAINT `FK_3603CFB6DBB5E18A` FOREIGN KEY (`etat_console_id`) REFERENCES `etat` (`id`);
 
 --
 -- Contraintes pour la table `jeu`
 --
 ALTER TABLE `jeu`
-  ADD CONSTRAINT `FK_82E48DB54B89032C` FOREIGN KEY (`post_id`) REFERENCES `console` (`id`),
-  ADD CONSTRAINT `FK_82E48DB591BD595D` FOREIGN KEY (`postjeu_id`) REFERENCES `etat` (`id`);
+  ADD CONSTRAINT `FK_82E48DB53E65A52A` FOREIGN KEY (`etat_jeu_id`) REFERENCES `etat` (`id`),
+  ADD CONSTRAINT `FK_82E48DB54456D5FB` FOREIGN KEY (`console_jeu_id`) REFERENCES `console` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
