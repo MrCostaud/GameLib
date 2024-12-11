0f3b043c6bfcc7ec196e252b90caff5a0b26fd95
MrCostaud <simarikalban@gmail.com>
2024-12-11 10:37:45 +0100

    <amiibo> les requêtes SQL

--- includes/modele/gestion_bdd.php
+++ includes/modele/gestion_bdd.php
@@ -16,4 +16,28 @@ function getAcces($username, $password)
     }
 }
 
+function addAmiibo($etat, $nom, $img)
+{
+    require "connexion.php";
+    $sql = "insert into amiibo values($etat, $nom, $img)";
+    $exec = $bdd->prepare($sql);
+    $exec->execute();
+}
+
+function modifAmiibo($idAmiibo, $etat, $nom, $img) {
+    require "connexion.php";
+    $sql = "UPDATE amiibo SET etat_amiibo_id=$etat, nom_amiibo=$nom, img_amiibo=$img where id=$idAmiibo";
+    $exec = $bdd->prepare($sql);
+    $exec->execute();
+}
+
+function suppAmiibo($idAmiibo)
+{
+    require "connexion.php";
+    $sql = "delete from amiibo where id=$idAmiibo";
+    $exec = $bdd->prepare($sql);
+    $exec->execute();
+}
+
+
 ?>
